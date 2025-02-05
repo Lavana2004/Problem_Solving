9b01cee14b04e3df1937e376ea6bb334c9847992
Lavana V <125808947+Lavana2004@users.noreply.github.com>
2025-02-05 19:40:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (19.06%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,10 @@
+#include <ranges>
+class Solution {
+public:
+    bool areAlmostEqual(string& s1, string& s2) {
+        vector<pair<char, char>> D;
+        for (pair<char, char>&& xy : views::zip(s1, s2))
+            if (xy.first!=xy.second) D.push_back(xy);
+        return D.size()==0 ||( D.size()==2 && D[0].first==D[1].second && D[0].second==D[1].first);
+    }
+};
